f25b8c9d52c2c8350aa5949cb85f7041562e382c
Maxim Sorkin <maximsorkin@cmail.carleton.ca>
2026-03-23 22:05:31 -0400

    add ffmpeg-chromium-opera-gx

---
+++ multimedia/codecs/ ffmpeg-chromium-opera-gx/actions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Created For Solus Operating System
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/usr"]
+IgnoreAutodep = True
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("unzip 0.%s-linux-x64.zip" % (get.srcVERSION()))
+    shelltools.system("unzip -oj %s_%s_linux_%s.crx3" % (WidevineName, WidevineVersion, WidevineSuffix)
+
+    # Create widevine_config.json
+    shelltools.system("printf '[\n      {\n         \"preload\": \"/usr/lib64/opera-gx-stable/lib_extra/WidevineCdm\"\n      }\n]\n' > widevine_config.json")
+
+    # create directories if not exist
+    shelltools.system("mkdir -p /usr/lib64/opera-gx-stable/lib_extra/WidevineCdm/_platform_specific/linux_x64")
+    shelltools.system("mkdir -p /usr/lib64/opera-gx-stable/resources")
+
+def install():
+    # set libraries and manifest permissions
+    shelltools.system("chmod 0644 libffmpeg.so")
+    shelltools.system("chmod 0644 libwidevinecdm.so")
+    shelltools.system("chmod 0644 manifest.json")
+
+    # place libraries and json files
+    pisitools.insinto("/usr/lib64/opera-gx-stable/lib_extra/", "libffmpeg.so")
+    pisitools.insinto("/usr/lib64/opera-gx-stable/lib_extra/WidevineCdm/_platform_specific/linux_x64/", "libwidevinecdm.so")
+    pisitools.insinto("/usr/lib64/opera-gx-stable/lib_extra/WidevineCdm/", "manifest.json")
+    pisitools.insinto("/usr/lib64/opera-gx-stable/resources/", "widevine_config.json")
