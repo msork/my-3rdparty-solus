ab541f80be36cf81069da3b718bb0d220211bd6c
Maxim Sorkin <maximsorkin@cmail.carleton.ca>
2026-03-23 22:17:30 -0400

    update the update-opera-gx-stable-solus.sh script

--- scripts/update-opera-gx-stable-solus.sh
+++ scripts/update-opera-gx-stable-solus.sh
@@ -8,17 +8,12 @@ sudo rm -rf ~/Downloads/opera-gx-stable-updater
 mkdir -p ~/Downloads/opera-gx-stable-updater
 cd ~/Downloads/opera-gx-stable-updater
 
-# DOWNLOAD RPM AND EXTRACT IT
-wget 'https://rpm.opera.com/rpm/'$FILE -O $FILE
+# DOWNLOAD DEB AND EXTRACT IT
 wget 'https://deb.opera.com/opera/pool/non-free/o/opera-gx-stable/'$DEBFILE -O $DEBFILE
-rpm2cpio $FILE | cpio -idmv
-
-# DOWNLOAD DEB AND EXTRACT VERSION
 ar -xvf $DEBFILE
 tar -xvf control.tar.xz
 VERSION=$(sed -n 2p control | grep -Po '(?<=Version: )\d+(?:\.\d+)*' | cut -d' ' -f2)
-rm debian-binary data.tar.xz control control.tar.xz $DEBFILE $FILE
-rm -rf usr
+rm debian-binary data.tar.xz control control.tar.xz $DEBFILE
 
 # COPY SCRIPT AND FILES
 wget 'https://raw.githubusercontent.com/msork/my-3rdparty-solus/refs/heads/master/scripts/ep-update.py' -O ep-update.py
@@ -29,14 +24,19 @@ wget 'https://raw.githubusercontent.com/msork/my-3rdparty-solus/refs/heads/maste
 chmod +x ep-update.py
 ./ep-update.py $VERSION "https://rpm.opera.com/rpm/$FILE"
 
-# BUILD AND INSTALL EOPKG
+# BUILD EOPKG
 sudo eopkg.py3 bi --ignore-safety pspec.xml
-sudo eopkg it -y ./*.eopkg
 
 # UPDATE PSPEC.XML
 cp pspec.xml $HOME/pkgs/my-3rdparty-solus/network/web/browser/opera-gx-stable/pspec.xml
 echo "REMEMBER TO git add . -> git commit -m 'Updated opera-gx-stable' -> git push -u origin master"
 
+# COPY EOPKG
+cp ~/Downloads/opera-gx-stable-updater/opera-gx-stable-$VERSION.eopkg ~/Downloads/opera-gx-stable-$VERSION.eopkg
+
 # REMOVE FILES
 sudo rm -rf ~/Downloads/opera-gx-stable-updater
 sudo rm ~/Downloads/opera-gx-stable-updater/opera-gx-stable-*.eopkg
+
+# ECHO REMINDER TO INSTALL FFMPEG
+echo "REMEMBER TO run the update-ffmpeg-chromium-opera-gx.sh script before you install, if you haven't already."
